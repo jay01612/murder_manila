1158eaa741c4da7aec0c9645f3ac630e546d0cdd
jay castillo <jaycastillo01612@gmail.com>
2021-01-14 11:37:05 -0800

    cors

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -8,7 +8,8 @@ class Cors
 	public function handle($request, Closure $next){
 		return $next($request)
         ->header('Access-Control-Allow-Origin', '*')
-		->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+		->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+		->header('Access-Control-Allow-Headers', 'Content-Type, X-Auth-Token, Origin, Authorization');
 		
 	}
 	// 	 header("Access-Control-Allow-Origin: *");
